d2fbc5f8a8ff4b84674c0b416324143d923a0b80
KyleRattet <krattet@gmail.com>
2015-12-15 22:20:33 -0700

    updated index with radial gauge structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,33 @@
-// sample angular code
+var app = angular.module("MyApp", ["wijmo"]);
+    function MyController($scope) {
 
-var app = angular.module('myApp', []);
+        $scope.propsStart = {
+          valueStart: 0,
+          valueEnd: 0,
+          max:360,
+          min: 0,
+          isUpdating:true,
+          ranges: {
+              one: { min: 0, max: 90, color: 'yellow' },
+              two: { min: 90, max: 180, color: 'orange' },
+              three: { min: 180, max: 270, color: 'red' },
+              four: { min: 270, max: 360, color: 'gray' }
+          },
+          labels: {
+              one: "Hi"
+          },
 
-app.controller('myController', ['$scope', function($scope) {
-  $scope.greeting = "Hello World!";
-}]);
+        };
+
+
+        $scope.directions = [
+          {name: "North", value: "0"},
+          {name: "Northeast", value: "45"},
+          {name: "East", value: "90"},
+          {name: "Southeast", value: "135"},
+          {name: "South", value: "180"},
+          {name: "Southwest", value: "225"},
+          {name: "West", value: "270"},
+          {name: "Northwest", value: "315"},
+        ];
+    }
